f288c89535924194f4216da390b539c6651bf05c
mrsantacruz86 <abnerdc86@gmail.com>
2018-03-16 18:43:58 -0400

    adding more html/css changes and the firebase database link

---
+++ assets/js/firebase.js
@@ -0,0 +1,29 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCyNP4-ntc37bN0OtkDNldtNnUoSNSx-S0",
+    authDomain: "next-mov-e.firebaseapp.com",
+    databaseURL: "https://next-mov-e.firebaseio.com",
+    projectId: "next-mov-e",
+    storageBucket: "",
+    messagingSenderId: "903599019806"
+  };
+  firebase.initializeApp(config);
+
+	// Authentication
+	// ________________________________________________________________________________________
+  // Create user with email and password
+  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+	});
+	
+
+	// Sign-In user with email and password
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
