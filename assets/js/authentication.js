7d79a6f6a9d7fe0a0e3272081710dc3796f31864
Nelson Diaz <abnerdc86@gmail.com>
2018-03-19 10:39:35 -0400

    updating authentication

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -3,32 +3,39 @@ var email;
 var password;
 var user;
 
+function login(evt) {
+	evt.preventDefault();
+	email = $('#username').val();
+	password = $('#password').val();
+
+	// Firebase Sign-In user with email and password
+	// Session persistent autentication method
+	user = firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+		.then(function () {
+			// Existing and future Auth states are now persisted in the current
+			// session only. Closing the window would clear any existing state even
+			// if a user forgets to sign out.
+			// ...
+			// New sign-in will be persisted with session persistence.
+			return firebase.auth().signInWithEmailAndPassword(email, password);
+		})
+		.then(function(status) {
+			console.log("status is: ", status)
+
+		})
+		.catch(function (error) {
+			// Handle Errors here.
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			console.log(errorCode + "\n" + errorMessage);
+		});
+}
+
+var loginElement = document.getElementById("login")
+loginElement.onclick = login;
 
 
 
 $(document).ready(function () {
 	//onClick event for the Login Button
-	$('#loginButton').click(function (event) {
-		event.preventDefault();
-		email = $('#username').val();
-		password = $('#password').val();
-
-		// Firebase Sign-In user with email and password
-		// Session persistent autentication method
-		user = firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-			.then(function () {
-				// Existing and future Auth states are now persisted in the current
-				// session only. Closing the window would clear any existing state even
-				// if a user forgets to sign out.
-				// ...
-				// New sign-in will be persisted with session persistence.
-				return firebase.auth().signInWithEmailAndPassword(email, password);
-			})
-			.catch(function (error) {
-				// Handle Errors here.
-				var errorCode = error.code;
-				var errorMessage = error.message;
-				console.log(errorCode + "\n" + errorMessage);
-			});
-	});
 });
